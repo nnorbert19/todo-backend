4a5265dc4f78b8f7d66d90d6d6ea86e979235c0e
nnorbert19 <norb0919@gmail.com>
2023-01-19 12:46:28 +0100

    fix?

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://127.0.0.1:5173",
-      "https://comfy-genie-b91d00.netlify.app/",
-    ],
+    origin: ["https://comfy-genie-b91d00.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(cookieParser());
